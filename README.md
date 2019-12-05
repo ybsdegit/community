29fd468b6170811c4148c33c84eaeec3e0a62694
ybsdegit <ybsdeyx@163.com>
2019-12-06 00:42:48 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Spring boot
-## init 
+### first day init  
+
+### 资料
+- Bootstrap: https://v3.bootcss.com/css/
+- Springboot: https://spring.io/guides
--- src/main/java/com/ybs/community/controller/HelloController.java
+++ src/main/java/com/ybs/community/controller/HelloController.java
@@ -19,4 +19,10 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello";
     }
+
+    @GetMapping("/index")
+    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
 }
