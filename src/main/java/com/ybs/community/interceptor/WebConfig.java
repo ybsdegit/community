6213fe98d069de5ac510a189ce815685e51b1f3f
ybsdegit <ybsdeyx@163.com>
2019-12-22 16:51:16 +0800

    拦截器

---
+++ src/main/java/com/ybs/community/interceptor/WebConfig.java
@@ -0,0 +1,29 @@
+package com.ybs.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * WebConfig
+ *
+ * @author Paulson
+ * @date 2019/12/22 16:21
+ */
+
+@Configuration
+//@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
+
+    }
+}
